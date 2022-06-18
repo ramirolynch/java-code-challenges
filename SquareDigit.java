1e60f95eef3fdb77eb11981f45955772d6d8b212
ramirolynch <ramirolynch@yahoo.com>
2022-06-18 12:52:32 -0500

    removed spaces

--- SquareDigit.java
+++ SquareDigit.java
@@ -7,20 +7,17 @@ public class SquareDigit {
           String strDigits = String.valueOf(n);    
           String result = "";
           
-          
-          
           // "for-each" loop to loop through elements in array of characters 
           // strDigits.toCharArray()
           
           for (char c : strDigits.toCharArray()) {
-              
-              
+                      
             // digit is a method of the object class character that returns the numeric value
             // of a character
               
             int digit = Character.digit(c, 10);
             
-            // here we are adding the square of digit to the empty string declared in line 9
+            // here we are adding the square of digit to the empty string declared in line 8
             // 
             result += digit * digit;
           }

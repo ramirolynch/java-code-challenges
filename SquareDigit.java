0cafabef5f782ac913de2471630f3ac27d5339ee
ramirolynch <ramirolynch@yahoo.com>
2022-06-18 12:50:57 -0500

    added SquareDigit.java file

---
+++ SquareDigit.java
@@ -0,0 +1,31 @@
+package com.ramirolynch;
+
+public class SquareDigit {
+    
+        public static int squareDigits(int n) {
+        
+          String strDigits = String.valueOf(n);    
+          String result = "";
+          
+          
+          
+          // "for-each" loop to loop through elements in array of characters 
+          // strDigits.toCharArray()
+          
+          for (char c : strDigits.toCharArray()) {
+              
+              
+            // digit is a method of the object class character that returns the numeric value
+            // of a character
+              
+            int digit = Character.digit(c, 10);
+            
+            // here we are adding the square of digit to the empty string declared in line 9
+            // 
+            result += digit * digit;
+          }
+          
+          return Integer.parseInt(result);
+        }
+
+}

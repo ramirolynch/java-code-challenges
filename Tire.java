5449d7a91c525d193a91c7bc3b5174de533f4d9f
ramirolynch <ramirolynch@yahoo.com>
2022-06-27 21:02:13 -0500

    this is how to implement an interface

---
+++ Tire.java
@@ -0,0 +1,16 @@
+package com.ramirolynch;
+
+// this is in its own Bounceable.java file:
+
+//public interface Bounceable {
+//    void bounce();
+//    void setBounceFactor(int bf);
+//}
+
+
+public class Tire implements Bounceable {
+    
+    public void bounce() {}
+    public void setBounceFactor(int bf) {}
+
+}
